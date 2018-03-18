8e1737d8718523120dbc354032fa6b20c72ea292
Sergey Tselovalnikov <sergeicelov@gmail.com>
2018-03-18 22:56:38 +1100

    fixes

--- src/test/kotlin/me/serce/ORMapTest.kt
+++ src/test/kotlin/me/serce/ORMapTest.kt
@@ -6,27 +6,23 @@ import org.junit.jupiter.api.Test
 
 internal class ORMapTest {
 
-  lateinit var global: Global<ORMap<Int, DotSet, AWSet<String>>>
-  lateinit var map1: AntiEntropy<ORMap<Int, DotSet, AWSet<String>>>
-  lateinit var set2: AntiEntropy<ORMap<Int, DotSet, AWSet<String>>>
+  lateinit var global: Global<ORMap<Int, DotSet, EWFlag>>
+  lateinit var map1: AntiEntropy<ORMap<Int, DotSet, EWFlag>>
+  lateinit var set2: AntiEntropy<ORMap<Int, DotSet, EWFlag>>
 
   @BeforeEach
   fun setUp() {
     global = Global()
-    map1 = global.create { ORMap<Int, DotSet, AWSet<String>>() }
-    set2 = global.create { ORMap<Int, DotSet, AWSet<String>>() }
+    map1 = global.create { ORMap<Int, DotSet, EWFlag>() }
+    set2 = global.create { ORMap<Int, DotSet, EWFlag>() }
   }
 
   @Test
   fun testMe() {
-//    map1.operation(Apply<Int, DotSet, Unit, Mutator<AWSet<String>, Unit>, AWSet<String>>({ DotSet() }),
-//      Triple(1, object: Mutator<AWSet<String>, Unit> {
-//        override fun apply(id: ID, state: AWSet<String>, args: Unit): AWSet<String> {
-//          return null!!
-//        }
-//      }, Unit))
-    assertEquals(setOf<Int>(), map1.query(Elements()))
-    assertEquals(setOf<Int>(), set2.query(Elements()))
+    map1.operation(Apply<Int, DotSet, Unit, Mutator<EWFlag, Unit>, EWFlag>({ DotSet() }),
+      Triple(1, Enable, Unit))
+    println(map1)
+
 
   }
 }
